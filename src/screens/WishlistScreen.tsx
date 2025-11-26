f27fe4c57fc6b7823dd23e40d0a6abc023dd8fa5
noumantahir <nouman.hawkdev@gmail.com>
2025-11-26 19:11:03 +0500

    rendering items in wishlist screen

--- src/screens/WishlistScreen.tsx
+++ src/screens/WishlistScreen.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import {View, StyleSheet, Button} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, { useMemo } from 'react';
+import { View, StyleSheet, FlatList, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useWishlistStore } from '../stores/wishlistStore';
+import { MovieHeader } from '../components/organisms';
+import { getImageUrl } from '../services/tmbd';
+import { WishlistEntry } from '../types/wishlist';
 
 export function WishlistScreen() {
   const navigation = useNavigation<any>();
+  const wishlist = useWishlistStore(state => state.wishlist);
+  const wishlistArray = useMemo(() => Array.from(wishlist.values()), [wishlist]);
+
+  const handleItemPress = (movieId: number) => {
+    navigation.navigate('Details', { movieId });
+  };
+
+
+  const renderItem = ({ item }: { item: WishlistEntry }) => {
+    const posterUrl = getImageUrl(item.poster_path || '', 'w500')
+
+    return (
+      <Pressable onPress={() => handleItemPress(item.id)}>
+        <MovieHeader title={item.title} posterUrl={posterUrl} />
+      </Pressable>
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+      <FlatList
+        data={wishlistArray}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        contentContainerStyle={styles.listContent}
       />
     </View>
   );
@@ -18,9 +41,9 @@ export function WishlistScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
+    backgroundColor: '#fff',
+  },
+  listContent: {
+    paddingBottom: 16,
   },
 });
-
