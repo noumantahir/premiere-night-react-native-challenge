dad61598a74b2e209083c7848e723158635ec68c
noumantahir <nouman.hawkdev@gmail.com>
2025-11-26 22:35:13 +0500

    added empty wishlist content

--- src/screens/WishlistScreen.tsx
+++ src/screens/WishlistScreen.tsx
@@ -3,7 +3,7 @@ import { View, StyleSheet, FlatList, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useWishlistStore } from '../stores/wishlistStore';
 import { MovieHeader } from '../components/organisms';
-import { IconButton } from '../components/atoms';
+import { IconButton, Text } from '../components/atoms';
 import { colors } from '../theme/colors';
 import { getImageUrl } from '../services/tmbd';
 import { WishlistEntry } from '../types/wishlist';
@@ -40,6 +40,17 @@ export function WishlistScreen() {
     );
   };
 
+  const renderEmptyList = () => (
+    <View style={styles.emptyContainer}>
+      <Text preset="subheading" style={styles.emptyText}>
+        Your wishlist is empty
+      </Text>
+      <Text preset="body" style={styles.emptySubtext}>
+        Add movies to your wishlist to see them here
+      </Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -47,6 +58,7 @@ export function WishlistScreen() {
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
         contentContainerStyle={styles.listContent}
+        ListEmptyComponent={renderEmptyList}
       />
     </View>
   );
@@ -59,6 +71,22 @@ const styles = StyleSheet.create({
   },
   listContent: {
     paddingBottom: 16,
+  },
+  emptyListContent: {
+    flexGrow: 1,
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 128,
+  },
+  emptyText: {
+    textAlign: 'center',
+    marginBottom: 8,
+  },
+  emptySubtext: {
+    textAlign: 'center',
+    color: colors.textSecondary,
   },
   itemContainer: {
     position: 'relative',
