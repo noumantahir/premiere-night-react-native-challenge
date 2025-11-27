b81be94d36fecda84843a8b5aac4b071ed733407
noumantahir <nouman.hawkdev@gmail.com>
2025-11-27 12:43:42 +0500

    refactored home screen into directory

---
+++ src/screens/HomeScreen/HomeScreen.styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../theme/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContent: {
+    paddingBottom: 16,
+  },
+});
+
